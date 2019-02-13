d353226d4b0e08cc45c3f46ad5c6b9df7d9590ba
Albert Pham <apham@defibtech.com>
2019-02-13 12:36:43 -0500

    Chapter 15- Entity Classes

---
+++ VideoOnDemand/Entities/Instructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoOnDemand.Entities
+{
+    public class Instructor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(1024), Required]
+        public string Name { get; set; }
+        
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
+        [MaxLength(1024)]
+        public string Thumbnail { get; set; }
+
+        public List<Course> Courses { get; set; }
+    }
+}
